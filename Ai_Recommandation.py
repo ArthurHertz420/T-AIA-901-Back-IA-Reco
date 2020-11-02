03f5e4310d1cd460c21b3f13e12e244255799137
touria oudadsse <toudadsse@gmail.com>
2020-11-02 04:33:06 +0100

    modify: correct classification words

--- Ai_Recommandation.py
+++ Ai_Recommandation.py
@@ -16,7 +16,7 @@ def word_list(textWords):
     return wordList
 
 # Lire les données depuis le fichier CSV
-inputGoodWords = pd.read_csv("resultat.csv").head(5)
+inputGoodWords = pd.read_csv("resultat.csv")
 inputBadWords = pd.read_csv("badwords.csv")
 
 goodWords = word_list(inputGoodWords['body_text_nostop'])
@@ -25,15 +25,15 @@ badWords = word_list(inputBadWords['body_text_nostop'])
 
 uniqueWords = set(goodWords).union(set(badWords))
 
-#classifier les good words depuis l'union entre les deux fichiers (resultat et badwords)
-numOfGoodWords = dict.fromkeys(uniqueWords, 0)
-for word in goodWords:
-    numOfGoodWords[word] = 1
-
 #classifier les bad words depuis l'union entre les deux fichiers (resultat et badwords)
 numOfBadWords = dict.fromkeys(uniqueWords, 0)
 for word in badWords:
     numOfBadWords[word] = 1
 
-print(numOfBadWords)
+#classifier les good words depuis l'union entre les deux fichiers (resultat et badwords)
+numOfGoodWords = dict.fromkeys(uniqueWords, 0)
+for word in goodWords:
+    numOfGoodWords[word] = 1
+
+print(numOfGoodWords)
 
