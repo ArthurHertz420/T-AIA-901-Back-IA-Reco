f275be8f933794ec669edd4b7cebf31b9e3c0e23
touria oudadsse <toudadsse@gmail.com>
2020-11-02 04:17:14 +0100

    add classification words

--- Ai_Recommandation.py
+++ Ai_Recommandation.py
@@ -1,17 +1,39 @@
-# Import Pandas
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import string
 
-# Load Good Data
-goodData = pd.read_csv('resultat.csv')
-goodData.head()
+# Function pour formater le text csv
+def word_list(textWords):
+    df = pd.DataFrame(textWords, columns=['body_text_nostop'])
+    wordList = df.stack().values.tolist()
+    wordList = str(wordList).replace("[", "")
+    wordList = str(wordList).replace("]", "")
+    wordList = str(wordList).replace(" ", "")
+    wordList = str(wordList).replace("'", "")
+    wordList = str(wordList).replace("\"", "")
+    wordList = wordList.split(',')
+    return wordList
 
-# Load Bad Data
-badData = pd.read_csv('bad_words.csv')
-badData.head()
+# Lire les données depuis le fichier CSV
+inputGoodWords = pd.read_csv("resultat.csv").head(5)
+inputBadWords = pd.read_csv("badwords.csv")
 
-#merger des de fichier
-data = pd.merge(goodData, badData, on='dataid')
-data.head()
+goodWords = word_list(inputGoodWords['body_text_nostop'])
 
-data.groupby('id')['body_text_nostop'].mean().head()
-data.groupby('id')['body_text_nostop'].mean().sort_values(ascending=False).head()
+badWords = word_list(inputBadWords['body_text_nostop'])
+
+uniqueWords = set(goodWords).union(set(badWords))
+
+#classifier les good words depuis l'union entre les deux fichiers (resultat et badwords)
+numOfGoodWords = dict.fromkeys(uniqueWords, 0)
+for word in goodWords:
+    numOfGoodWords[word] = 1
+
+#classifier les bad words depuis l'union entre les deux fichiers (resultat et badwords)
+numOfBadWords = dict.fromkeys(uniqueWords, 0)
+for word in badWords:
+    numOfBadWords[word] = 1
+
+print(numOfBadWords)
+
